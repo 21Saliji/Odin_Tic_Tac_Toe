12524807f45da89afad5634a2e4318883ebc0423
21Saliji <chantelsaliji@gmail.com>
2025-03-14 12:36:34 +0200

    working on start button

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const container = document.querySelector('#grid');
-for (let i = 1; i <= 9; i++) {
-  const item = document.createElement('div');
-  item.textContent = i;
-  container.appendChild(item);
-}
+const startButton = document.querySelector('#start');
+startButton.addEventListener("click", ()=>{
+  alert("hellooo")
+})
+
+//const container = document.querySelector('#grid');
+// for (let i = 1; i <= 9; i++) {
+//   const item = document.createElement('div');
+//   item.textContent = i;
+//   container.appendChild(item);
+// }
